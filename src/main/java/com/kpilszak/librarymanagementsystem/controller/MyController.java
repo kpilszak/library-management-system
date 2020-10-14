38bf8916a1768364de7ae9bb662cd8aa0863b75c
kpilszak <klaudia.pilszak@gmail.com>
2020-10-14 21:47:32 +0200

    feat: Add Book entity + feat: Create packages

--- src/main/java/com/kpilszak/librarymanagementsystem/controller/MyController.java
+++ src/main/java/com/kpilszak/librarymanagementsystem/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.kpilszak.librarymanagementsystem;
+package com.kpilszak.librarymanagementsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
