4ae975cc1e0f95f3eb6ddd5b5c54b12faac55d13
kpilszak <klaudia.pilszak@gmail.com>
2020-10-17 22:04:34 +0200

    feat: Add Order Repository

---
+++ src/main/java/com/kpilszak/librarymanagementsystem/data/model/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.kpilszak.librarymanagementsystem.data.model;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from Order c where c.booking_id = ?1")
+    void deleteByOrderId(String orderId);
+}
