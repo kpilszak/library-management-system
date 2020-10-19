fa34b590b99e5d36147ceb6bb0233432d89a6cd8
kpilszak <klaudia.pilszak@gmail.com>
2020-10-19 20:23:29 +0200

    feat: Add Controller to login, register, search

---
+++ src/main/java/com/kpilszak/librarymanagementsystem/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.kpilszak.librarymanagementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/Login")
+    public String getRequest() {
+        System.out.println("returning call from Login user...");
+        return "home";
+    }
+
+    @GetMapping("/register")
+    public String registerUser() {
+        System.out.println("returning call from Login user...");
+        return "register";
+    }
+
+    @GetMapping("/search")
+    public String searchDetails() {
+        System.out.println("returning call from Login user...");
+        return "search";
+    }
+}
